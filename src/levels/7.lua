3c4e208c0bb1385e82e14407503243d460c5867c
Seppi <josefnpat@gmail.com>
2017-02-20 14:22:39 -0600

    Hooking up adj voiceovers

--- src/levels/7.lua
+++ src/levels/7.lua
@@ -3,7 +3,7 @@ local level = {}
 level.intro = function(self)
   local tvn = libs.vn.new()
   tvn:addFrame(vn.com.default,nil,"Commander","Warning: hostiles detected...")
-  tvn:addFrame(vn.adj.default,vn.adj.overlay,"Adjutant","That is incorrect.")
+  tvn:addFrame(vn.adj.default,vn.adj.overlay,"Adjutant","That is incorrect.",vn_audio.adj.incorrect)
   tvn:addFrame(vn.com.default,nil,"Commander","Woah, no enemies? That’s new … better stockpile while I can!")
   tvn:addFrame(nil,nil,"[TIP]","Press left-ctrl + <number> to assign a control group. Use <number> to select that control group.")
   return tvn
