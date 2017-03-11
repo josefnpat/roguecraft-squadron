d9b6eb0d0b93d1d7bfd4b8cbbf0a5322687f18a6
Seppi <josefnpat@gmail.com>
2017-03-11 12:33:46 -0600

    Adding another asteroid

--- src/assets/objects_data/asteroid.lua
+++ src/assets/objects_data/asteroid.lua
@@ -1,7 +1,7 @@
 return function()
   return {
     type = "asteroid",
-    variation = math.random(0,3),
+    variation = math.random(0,4),
     size = 32,
     ore_supply = math.random(75,125),
   }
