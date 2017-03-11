700ebb5017fc742f681ee8bbde5c89d41e001e76
Seppi <josefnpat@gmail.com>
2017-03-11 08:59:58 -0600

    Adding new asteroids with old style

--- src/assets/objects_data/asteroid.lua
+++ src/assets/objects_data/asteroid.lua
@@ -1,7 +1,7 @@
 return function()
   return {
     type = "asteroid",
-    variation = math.random(0,1),
+    variation = math.random(0,3),
     size = 32,
     ore_supply = math.random(75,125),
   }
