f57a2acd5b5e4681fce9bfe00a6596a88c61c7fb
Seppi <josefnpat@gmail.com>
2018-03-04 15:51:55 -0600

    Cleaning up version info

--- src/libs/version.lua
+++ src/libs/version.lua
@@ -1,26 +1,53 @@
 local version = {}
 
 function version.draw()
-  local vs_info = libs.i18n('git.client.info',{git_count=git_count,git_hash=git_hash}).."\n"
+  local vs_info = ""
   if version_server then
-    if version_server.count and version_server.hash then
-      vs_info = vs_info .. libs.i18n('git.server.info',{
-        git_count=version_server.count,
-        git_hash=version_server.hash}).."\n"
-    end
+
     if version_server.message then
-      vs_info = vs_info .. libs.i18n('git.server.message')..": ".. tostring(version_server.message).."\n"
+      vs_info = vs_info .. libs.i18n('git.server.message')..": "..
+        tostring(version_server.message).."\n"
     end
     if version_server.error then
-      vs_info = vs_info .. libs.i18n('git.server.error') .. ": ".. tostring(version_server.error).."\n"
+      vs_info = vs_info .. libs.i18n('git.server.error') .. ": "..
+        tostring(version_server.error).."\n"
     end
+
     if version_server.count > git_count then
-      vs_info = vs_info .. libs.i18n('git.behind')
+      vs_info = vs_info .. libs.i18n('git.behind').."\n"
     elseif version_server.count < git_count then
-      vs_info = vs_info .. libs.i18n('git.ahead')
+      vs_info = vs_info .. libs.i18n('git.ahead').."\n"
     else -- ==
-      vs_info = vs_info .. libs.i18n('git.equal')
+      vs_info = vs_info .. libs.i18n('git.equal').."\n"
     end
+
+    if version_server.count == git_count then
+
+      vs_info = vs_info .. libs.i18n('git.both.info',{
+        git_count=git_count,
+        git_hash=git_hash
+      }).."\n"
+
+    else
+
+      vs_info = vs_info .. libs.i18n('git.client.info',{
+        git_count=git_count,
+        git_hash=git_hash
+      }).."\n"
+
+      if version_server.count and version_server.hash then
+        vs_info = vs_info .. libs.i18n('git.server.info',{
+          git_count=version_server.count,
+          git_hash=version_server.hash}).."\n"
+      end
+
+    end
+
+  else
+    vs_info = vs_info .. libs.i18n('git.both.info',{
+        git_count=git_count,
+        git_hash=git_hash
+      }).."\n"
   end
   dropshadowf(vs_info,
     32,32,love.graphics.getWidth()-64,"left")
