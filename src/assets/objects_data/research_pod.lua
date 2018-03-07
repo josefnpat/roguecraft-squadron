39c4bbc030fbeec54c669f8aecabd1cf4b34b847
Seppi <josefnpat@gmail.com>
2018-03-06 19:26:09 -0600

    Updating research pod graphics

--- src/assets/objects_data/research_pod.lua
+++ src/assets/objects_data/research_pod.lua
@@ -2,6 +2,7 @@ return function()
   return {
     type = "research_pod",
     size = 32,
+    variation = math.random(1,100) == 1 and 1 or 0,
     rotate = (math.random(0,1)*2-1)/10,
     minimap = false,
     research = 1,
