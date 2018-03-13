41d6a63c8661cb01cac107e21f8dea577b6e8b5c
Seppi <josefnpat@gmail.com>
2018-03-13 01:51:40 -0500

    bar color to match mockups

--- src/libs/bar.lua
+++ src/libs/bar.lua
@@ -16,7 +16,7 @@ function barlib.new(init)
   self._width = init.width or 256
   self._height = init.height or barlib.img.corner:getHeight()+self._padding*2
   self._color = init.color or {0,255,255}
-  self._barColor = init.color or {255,255,0,191}
+  self._barColor = init.color or {246,197,42}
   self._textColor = init.textColor or {0,0,0}
   self._textInverseColor = init.textInverseColor or {255,255,255}
   self._icon = init.icon
