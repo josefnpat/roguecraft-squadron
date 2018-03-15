112e92a680d57835a05c40aede62b054d94f1c53
Seppi <josefnpat@gmail.com>
2018-03-15 09:16:44 -0500

    Removing some lines as per #289

--- src/assets/levels/3.lua
+++ src/assets/levels/3.lua
@@ -3,8 +3,8 @@ local level = {}
 level.intro = function(self)
   local tvn = libs.vn.new()
   tvn:addFrame(vn.adj.default,vn.adj.overlay,libs.i18n('vn.adj.base'),libs.i18n('vn.adj.warning'),vn_audio.adj.warning)
-  tvn:addFrame(vn.com.default,nil,libs.i18n('vn.com.base'),libs.i18n('vn.com.9'),vn_audio.com.line9)
-  tvn:addFrame(nil,nil,libs.i18n('vn.tip.base'),libs.i18n('vn.tip.3'))
+  --tvn:addFrame(vn.com.default,nil,libs.i18n('vn.com.base'),libs.i18n('vn.com.9'),vn_audio.com.line9)
+  --tvn:addFrame(nil,nil,libs.i18n('vn.tip.base'),libs.i18n('vn.tip.3'))
   return tvn
 end
 
--- src/assets/levels/5.lua
+++ src/assets/levels/5.lua
@@ -3,7 +3,7 @@ local level = {}
 level.intro = function(self)
   local tvn = libs.vn.new()
   tvn:addFrame(vn.adj.default,vn.adj.overlay,libs.i18n('vn.adj.base'),libs.i18n('vn.adj.warning'),vn_audio.adj.warning)
-  tvn:addFrame(vn.com.default,nil,libs.i18n('vn.com.base'),libs.i18n('vn.com.11'),vn_audio.com.line11)
+  --tvn:addFrame(vn.com.default,nil,libs.i18n('vn.com.base'),libs.i18n('vn.com.11'),vn_audio.com.line11)
   tvn:addFrame(nil,nil,libs.i18n('vn.tip.base'),libs.i18n('vn.tip.5'))
   return tvn
 end
