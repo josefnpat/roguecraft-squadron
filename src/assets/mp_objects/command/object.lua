a5d0c45e1231f26ebe11cc295d67a8dc7766800e
Seppi <josefnpat@gmail.com>
2018-05-28 22:55:03 -0500

    Adding missing mp_objects

---
+++ src/assets/mp_objects/command/object.lua
@@ -0,0 +1,22 @@
+return function()
+  return {
+    type = "command",
+    cost = {material=1000,crew=100},
+    fow = cheat and 32 or 0.5,
+    crew = 100,
+    size = 64,
+    speed = 50,
+    health = {max = 75,},
+    material = 1600,
+    repair = false,
+    actions = {
+      "salvage","repair",
+      "build_salvager",
+      "build_habitat",
+      "build_fighter",
+      "build_drydock",
+      "build_satellite",
+    },
+    build_time = 60,
+  }
+end
