031c7b5aa86643808ac31f2f15b2db95c6347aca
Seppi <josefnpat@gmail.com>
2018-06-29 22:09:06 -0500

    Ships only drop one debris now

--- src/assets/mp_objects/debris/object.lua
+++ src/assets/mp_objects/debris/object.lua
@@ -2,7 +2,7 @@ return function()
   return {
     type = "debris",
     size = 16,
-    material_supply = 25,
+    material_supply = 1, -- overridden
     rotate = (math.random(0,1)*2-1)/20,
     pc = false,
   }
--- src/states/server.lua
+++ src/states/server.lua
@@ -1045,11 +1045,8 @@ function server:update(dt)
         local cx,cy = libs.net.getCurrentLocation(object,love.timer.getTime())
         local object_type = libs.objectrenderer.getType(object.type)
         if object_type.cost and object_type.cost.material then
-          for i = 1,math.floor(object_type.cost.material/25/2) do
-            local ox = math.random(-object_type.size,object_type.size)
-            local oy = math.random(-object_type.size,object_type.size)
-            server.createObject(storage,"debris",cx+ox,cy+oy,nil)
-          end
+          local debris = server.createObject(storage,"debris",cx,cy,nil)
+          debris.material_supply = object_type.cost.material/2
         end
 
       end
