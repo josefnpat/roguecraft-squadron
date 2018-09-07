fbff0540999114ef411eabc0978a6669bedc7258
Seppi <josefnpat@gmail.com>
2018-09-07 18:34:15 -0500

    Cleanup

--- src/states/server.lua
+++ src/states/server.lua
@@ -801,7 +801,6 @@ function server:newGame()
     server.maps.spacedpockets.config)
 
   if self.lovernet:getStorage().config.ai then
-    --for _,ai in pairs(self.lovernet:getStorage().ai_players) do
     local ai_players = self.lovernet:getStorage().ai_players
     for ai_index = 1,self.lovernet:getStorage().config.ai do
       local ai = ai_players[ai_index]
