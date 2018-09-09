f8bb47efff79f2cfdb7244fc48c5527ac7a1acc6
Seppi <josefnpat@gmail.com>
2018-09-09 18:42:39 -0500

    Increasing cost of radar slightly

--- src/assets/mp_objects/radar/object.lua
+++ src/assets/mp_objects/radar/object.lua
@@ -1,12 +1,12 @@
 return function()
   return {
     type = "radar",
-    cost = {material=100,crew=5},
+    cost = {material=125,crew=5},
     fow = 2,
     crew = 5,
     size = 32,
     speed = 75,
-    health = {max = 10,},
+    health = {max = 13,},
     actions = {
       "build_satellite",
     },
