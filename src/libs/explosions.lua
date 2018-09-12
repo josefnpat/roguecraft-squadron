45ebc1b0451629ae10ca68b33a21efdb92030d4d
Seppi <josefnpat@gmail.com>
2018-09-12 16:18:06 -0500

    Adding new explosions!
    Relates to #401

--- src/libs/explosions.lua
+++ src/libs/explosions.lua
@@ -1,11 +1,16 @@
 local explosions = {}
 
-explosions.images = {}
-for i = 1,6 do
-  table.insert(
-    explosions.images,
-    love.graphics.newImage("assets/explosions/b"..i..".png")
-  )
+local explosion_dir = "assets/explosions/"
+
+explosions.assets = {}
+for _,exptype in pairs(love.filesystem.getDirectoryItems(explosion_dir)) do
+  local explosion = {}
+  local frame_explosion_dir = explosion_dir .. exptype .. "/"
+  for _,expframe in pairs(love.filesystem.getDirectoryItems(frame_explosion_dir)) do
+    local frame = frame_explosion_dir .. expframe
+    table.insert(explosion,love.graphics.newImage(frame))
+  end
+  table.insert(explosions.assets,explosion)
 end
 
 function explosions.new(init)
@@ -23,6 +28,7 @@ end
 
 function explosions:add(object,size)
   table.insert(self.data,{
+    type = math.random(#explosions.assets),
     x = object.dx + math.random(-size,size),
     y = object.dy + math.random(-size,size),
     angle = math.random()*math.pi*2,
@@ -32,14 +38,14 @@ end
 
 function explosions:update(dt)
   for _,e in pairs(self.data) do
-    e.dt = e.dt + dt*#explosions.images*4
+    e.dt = e.dt + dt*#explosions.assets[e.type]
   end
 end
 
 function explosions:draw()
   for ei,explosion in pairs(self.data) do
     local index = math.floor(explosion.dt)+1
-    local img = explosions.images[index]
+    local img = explosions.assets[explosion.type][index]
     if img then
       love.graphics.draw(img,
         explosion.x,explosion.y,
