216f7e3ee0cb8099cc07cbec83ec089506e516bc
Seppi <josefnpat@gmail.com>
2018-10-09 05:38:53 -0500

    AI Checks to see if they have any moving ships. If not, they surrender.
    closes #556

---
+++ src/libs/ai/actions/surrender.lua
@@ -0,0 +1,44 @@
+local action = {}
+
+action._check_t = 5
+
+function action.new(init)
+  init = init or {}
+  local self = {}
+  self.updateFixed = action.updateFixed
+  self.update = action.update
+  self._check_dt = math.random()*action._check_t
+  return self
+end
+
+function action:updateFixed(ai)
+  return {},0
+end
+
+function action:update(dt,ai)
+  self._check_dt = self._check_dt + dt
+  if self._check_dt >= action._check_t then
+    self._check_dt = 0
+    local user = ai:getUser()
+    local surrender = true
+    local player_objects = {}
+    for _,object in pairs(ai:getStorage().objects) do
+      if object.user == user.id then
+        table.insert(player_objects,object)
+        object_type = libs.objectrenderer.getType(object.type)
+        if object_type.speed then
+          surrender = false
+        end
+      end
+    end
+    if surrender then
+      for _,object in pairs(player_objects) do
+        ai:getServer():addUpdate(object,{remove=true},"delete_objects")
+        object.remove = true
+      end
+    end
+  end
+
+end
+
+return action
