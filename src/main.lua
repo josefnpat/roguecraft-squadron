6e881700d1c177bd712d70e8bfedff6c8abafc2e
Seppi <josefnpat@gmail.com>
2018-10-15 10:16:09 -0500

    Main changes for loading

--- src/main.lua
+++ src/main.lua
@@ -32,56 +32,68 @@ function love.load(arg)
     return
   end
 
-  love.window.setIcon(love.image.newImageData("assets/icon.png"))
+  loader = require"libs.loader".new{onDone=function()
+    libs.hump.gamestate.switch(states.splash)
+  end}
 
-  tooltipf_edge = love.graphics.newImage("assets/hud/tooltip_edge.png")
+  loader:add("window icon",function()
+    love.window.setIcon(love.image.newImageData("assets/icon.png"))
+  end)
 
-  vn = {
-    adj = {
-      default = love.graphics.newImage("assets/vn/adj/default.png"),
-      overlay = {
-        love.graphics.newImage("assets/vn/adj/shine1.png"),
-        love.graphics.newImage("assets/vn/adj/shine2.png")
+  loader:add("tooltip edge",function()
+    tooltipf_edge = love.graphics.newImage("assets/hud/tooltip_edge.png")
+  end)
+
+  loader:add("mission vn images",function()
+    vn = {
+      adj = {
+        default = love.graphics.newImage("assets/vn/adj/default.png"),
+        overlay = {
+          love.graphics.newImage("assets/vn/adj/shine1.png"),
+          love.graphics.newImage("assets/vn/adj/shine2.png")
+        },
       },
-    },
-    com = {
-      default = love.graphics.newImage("assets/vn/com/default.png"),
+      com = {
+        default = love.graphics.newImage("assets/vn/com/default.png"),
+      }
     }
-  }
+  end)
 
-  vn_audio = {
-    adj = {
-      correct = love.audio.newSource("assets/vn/adj/audio/correct.ogg","stream"),
-      incorrect = love.audio.newSource("assets/vn/adj/audio/incorrect.ogg","stream"),
-      warning = love.audio.newSource("assets/vn/adj/audio/warning.ogg","stream"),
-      line1 = love.audio.newSource("assets/vn/adj/audio/line1.ogg","stream"),
-      line2 = love.audio.newSource("assets/vn/adj/audio/line2.ogg","stream"),
-      line3 = love.audio.newSource("assets/vn/adj/audio/line3.ogg","stream"),
-      line4 = love.audio.newSource("assets/vn/adj/audio/line4.ogg","stream"),
-      line5 = love.audio.newSource("assets/vn/adj/audio/line5.ogg","stream"),
-      line6 = love.audio.newSource("assets/vn/adj/audio/line6.ogg","stream"),
-      line7 = love.audio.newSource("assets/vn/adj/audio/line7.ogg","stream"),
-      line8 = love.audio.newSource("assets/vn/adj/audio/line8.ogg","stream"),
-      --line9 = love.audio.newSource("assets/vn/adj/audio/line9.ogg","stream"),
-    },
-    com = {
-      line1 = love.audio.newSource("assets/vn/com/audio/line1.ogg","stream"),
-      line2 = love.audio.newSource("assets/vn/com/audio/line2.ogg","stream"),
-      line3 = love.audio.newSource("assets/vn/com/audio/line3.ogg","stream"),
-      line4 = love.audio.newSource("assets/vn/com/audio/line4.ogg","stream"),
-      line5 = love.audio.newSource("assets/vn/com/audio/line5.ogg","stream"),
-      line6 = love.audio.newSource("assets/vn/com/audio/line6.ogg","stream"),
-      line7 = love.audio.newSource("assets/vn/com/audio/line7.ogg","stream"),
-      line8 = love.audio.newSource("assets/vn/com/audio/line8.ogg","stream"),
-      line9 = love.audio.newSource("assets/vn/com/audio/line9.ogg","stream"),
-      line10 = love.audio.newSource("assets/vn/com/audio/line10.ogg","stream"),
-      line11 = love.audio.newSource("assets/vn/com/audio/line11.ogg","stream"),
-      line12 = love.audio.newSource("assets/vn/com/audio/line12.ogg","stream"),
-      line13 = love.audio.newSource("assets/vn/com/audio/line13.ogg","stream"),
-      line14 = love.audio.newSource("assets/vn/com/audio/line14.ogg","stream"),
-      line15 = love.audio.newSource("assets/vn/com/audio/line15.ogg","stream"),
-    },
-  }
+  loader:add("mission vn audio",function()
+    vn_audio = {
+      adj = {
+        correct = love.audio.newSource("assets/vn/adj/audio/correct.ogg","stream"),
+        incorrect = love.audio.newSource("assets/vn/adj/audio/incorrect.ogg","stream"),
+        warning = love.audio.newSource("assets/vn/adj/audio/warning.ogg","stream"),
+        line1 = love.audio.newSource("assets/vn/adj/audio/line1.ogg","stream"),
+        line2 = love.audio.newSource("assets/vn/adj/audio/line2.ogg","stream"),
+        line3 = love.audio.newSource("assets/vn/adj/audio/line3.ogg","stream"),
+        line4 = love.audio.newSource("assets/vn/adj/audio/line4.ogg","stream"),
+        line5 = love.audio.newSource("assets/vn/adj/audio/line5.ogg","stream"),
+        line6 = love.audio.newSource("assets/vn/adj/audio/line6.ogg","stream"),
+        line7 = love.audio.newSource("assets/vn/adj/audio/line7.ogg","stream"),
+        line8 = love.audio.newSource("assets/vn/adj/audio/line8.ogg","stream"),
+        --line9 = love.audio.newSource("assets/vn/adj/audio/line9.ogg","stream"),
+      },
+      com = {
+        line1 = love.audio.newSource("assets/vn/com/audio/line1.ogg","stream"),
+        line2 = love.audio.newSource("assets/vn/com/audio/line2.ogg","stream"),
+        line3 = love.audio.newSource("assets/vn/com/audio/line3.ogg","stream"),
+        line4 = love.audio.newSource("assets/vn/com/audio/line4.ogg","stream"),
+        line5 = love.audio.newSource("assets/vn/com/audio/line5.ogg","stream"),
+        line6 = love.audio.newSource("assets/vn/com/audio/line6.ogg","stream"),
+        line7 = love.audio.newSource("assets/vn/com/audio/line7.ogg","stream"),
+        line8 = love.audio.newSource("assets/vn/com/audio/line8.ogg","stream"),
+        line9 = love.audio.newSource("assets/vn/com/audio/line9.ogg","stream"),
+        line10 = love.audio.newSource("assets/vn/com/audio/line10.ogg","stream"),
+        line11 = love.audio.newSource("assets/vn/com/audio/line11.ogg","stream"),
+        line12 = love.audio.newSource("assets/vn/com/audio/line12.ogg","stream"),
+        line13 = love.audio.newSource("assets/vn/com/audio/line13.ogg","stream"),
+        line14 = love.audio.newSource("assets/vn/com/audio/line14.ogg","stream"),
+        line15 = love.audio.newSource("assets/vn/com/audio/line15.ogg","stream"),
+      },
+    }
+  end)
 
   difficulty = {
     mult = {
@@ -91,187 +103,180 @@ function love.load(arg)
     },
   }
 
-  function makeFonts()
-    fonts = {
-      default = love.graphics.newFont("assets/fonts/Yantramanav-Regular.ttf",16),
-      small = love.graphics.newFont("assets/fonts/Yantramanav-Regular.ttf",12),
-      large = love.graphics.newFont("assets/fonts/Yantramanav-Regular.ttf",24),
-      window_title = love.graphics.newFont("assets/fonts/ExpletusSans-Bold.ttf",20),
-      title = love.graphics.newFont("assets/fonts/ExpletusSans-Bold.ttf",64),
-      menu = love.graphics.newFont("assets/fonts/Yantramanav-Regular.ttf",20),
-      vn_name = love.graphics.newFont("assets/fonts/Yantramanav-Regular.ttf",48),
-      vn_text = love.graphics.newFont("assets/fonts/Yantramanav-Regular.ttf",24),
-      vn_info = love.graphics.newFont("assets/fonts/Yantramanav-Regular.ttf",16),
-      fallback = love.graphics.newFont("assets/fonts/NovaMono.ttf",16),
-    }
-  end
-  makeFonts()
-
-  love.graphics.setFont(fonts.default)
-  fonts.default:setFallbacks(fonts.fallback)
+  loader:add("fonts",function()
+    function makeFonts()
+      fonts = {
+        default = love.graphics.newFont("assets/fonts/Yantramanav-Regular.ttf",16),
+        small = love.graphics.newFont("assets/fonts/Yantramanav-Regular.ttf",12),
+        large = love.graphics.newFont("assets/fonts/Yantramanav-Regular.ttf",24),
+        window_title = love.graphics.newFont("assets/fonts/ExpletusSans-Bold.ttf",20),
+        title = love.graphics.newFont("assets/fonts/ExpletusSans-Bold.ttf",64),
+        menu = love.graphics.newFont("assets/fonts/Yantramanav-Regular.ttf",20),
+        vn_name = love.graphics.newFont("assets/fonts/Yantramanav-Regular.ttf",48),
+        vn_text = love.graphics.newFont("assets/fonts/Yantramanav-Regular.ttf",24),
+        vn_info = love.graphics.newFont("assets/fonts/Yantramanav-Regular.ttf",16),
+        fallback = love.graphics.newFont("assets/fonts/NovaMono.ttf",16),
+      }
+    end
+    makeFonts()
+    love.graphics.setFont(fonts.default)
+    fonts.default:setFallbacks(fonts.fallback)
+  end)
 
   libs = {
     hump = {
       gamestate = require "libs.gamestate",
       camera = require "libs.camera",
     },
-    healthcolor = require"libs.healthcolor",
-    healthbar = require"libs.healthbar",
-    splash = require "libs.splash",
-    vn = require"libs.vn",
-    stars = require"libs.stars",
-    score = require"libs.score",
-    menu = require"libs.menu",
-    pcb = require"libs.progresscirclebar",
-    window = require"libs.window",
-    tutorial = require"libs.tutorial",
-    json = require"libs.json",
-    notif = require"libs.notif",
-    tree = require"libs.tree",
-    assetchooser = require"libs.assetchooser",
-    stringchooser = require"libs.stringchooser",
-    i18n = require"libs.i18n",
-    gettext = require"libs.gettextlib",
     cursor = require"libs.cursor",
-    version = require"libs.version",
-    camera_edge = require"libs.camera_edge",
-    bar = require"libs.bar",
-    lovernet = require"libs.lovernet.lovernet",
-    lovernetprofiler = require"libs.lovernetprofiler",
-    bitser = require"libs.lovernet.bitser",
-    selection = require"libs.selection",
-    buildqueue = require"libs.buildqueue",
-    net = require"libs.net",
-    objectrenderer = require"libs.objectrenderer",
-    researchrenderer = require"libs.researchrenderer",
-    bulletrenderer = require"libs.bulletrenderer",
-    minimap = require"libs.minimap",
-    resources = require"libs.resources",
-    fow = require"libs.fow",
-    demo = require"libs.demo",
-    planets = require"libs.planets",
-    matrixpanel = require"libs.matrixpanel",
-    actionpanel = require"libs.actionpanel",
-    explosions = require"libs.explosions",
-    gather = require"libs.gather",
-    moveanim = require"libs.moveanim",
-    sfx = require"libs.sfx",
-    controlgroups = require"libs.controlgroups",
-    chat = require"libs.chat",
-    button = require"libs.button",
-    slider = require"libs.slider",
-    dynamicaudio = require"libs.dynamicaudio",
-    mpdisconnect = require"libs.mpdisconnect",
-    mpconnect = require"libs.mpconnect",
-    mpresearch = require"libs.mpresearch",
-    mpconnectplayer = require"libs.mpconnectplayer",
-    gamestatus = require"libs.gamestatus",
-    ring = require"libs.ring",
-    ai = require"libs.ai",
-    mppresets = require"libs.mppresets",
-    loading = require"libs.loading",
-    picocam = require"libs.picocam",
-    points = require"libs.points",
-    matchstats = require"libs.matchstats",
-    social = require"libs.social",
-    soundtrack = require"libs.soundtrack",
   }
 
-  libs.objectrenderer.load(true)
-  libs.researchrenderer.load(true)
-  libs.bulletrenderer.load(true)
-  libs.sfx.load()
+  loader:add("lib dependencies",function()
+    libs.gettext = require"libs.gettextlib"
+    libs.i18n = require"libs.i18n"
+    libs.lovernet = require"libs.lovernet.lovernet"
+    libs.bitser = require"libs.lovernet.bitser"
+    libs.ai = require"libs.ai"
+  end)
+
+  local lib_dir = "libs/"
+  for _,filename in pairs(love.filesystem.getDirectoryItems(lib_dir)) do
+    if love.filesystem.isFile( lib_dir..filename ) then
+      loader:add(file.name(filename),function()
+        if libs[file.name(filename)] == nil then
+          libs[file.name(filename)] = require(lib_dir..(file.name(filename)))
+        end
+      end)
+    end
+  end
+
+  loader:add("renderers",function()
+    libs.objectrenderer.load(true)
+    libs.researchrenderer.load(true)
+    libs.bulletrenderer.load(true)
+  end)
+
+  loader:add("a planet",function()
+    libs.stars:loadPlanet()
+  end)
+  loader:add("some space",function()
+    libs.stars:loadSpace()
+  end)
+  loader:add("some stars",function()
+    libs.stars:loadStars0()
+  end)
+  loader:add("some more stars",function()
+    libs.stars:loadStars1()
+  end)
+  loader:add("stars",function()
+    libs.stars:load()
+  end)
+
+  loader:add("sfx",function()
+    libs.sfx.load()
+  end)
 
   states = {
-    splash = require "states.splash",
-    menu = require "states.menu",
-    pause = require "states.pause",
-    options = require "states.options",
-    gameover = require"states.gameover",
-    tree = require"states.tree",
-    mission = require "states.mission",
-    credits = require "states.credits",
-    disclaimer = require "states.disclaimer",
-    debug = require "states.debug",
-    dynamicmusic = require "states.dynamicmusic",
-    client = require"states.client",
+    load = require "states.load",
     server = require"states.server",
   }
 
-  local loc_data = libs.gettext.decode(love.filesystem.read("assets/loc/en.po"))
+  loader:add("gamestates",function()
+    states = {
+      load = require "states.load",
+      splash = require "states.splash",
+      menu = require "states.menu",
+      pause = require "states.pause",
+      options = require "states.options",
+      gameover = require"states.gameover",
+      tree = require"states.tree",
+      mission = require "states.mission",
+      credits = require "states.credits",
+      disclaimer = require "states.disclaimer",
+      debug = require "states.debug",
+      dynamicmusic = require "states.dynamicmusic",
+      client = require"states.client",
+      server = require"states.server",
+    }
+  end)
 
-  --local loc_i18n = {}
-  for i,v in pairs(loc_data) do
-    local newstr = v.str:gsub([[\n]],"\n"):gsub([[\"]],"\"")
-    libs.i18n.set('en.'..v.id,newstr)
-  end
+  loader:add("localizations",function()
+    local loc_data = libs.gettext.decode(love.filesystem.read("assets/loc/en.po"))
+    for i,v in pairs(loc_data) do
+      local newstr = v.str:gsub([[\n]],"\n"):gsub([[\"]],"\"")
+      libs.i18n.set('en.'..v.id,newstr)
+    end
+  end)
 
-  libs.cursor.add("default","assets/hud/cursors/default.png")
-  libs.cursor.add("player","assets/hud/cursors/player.png")
-  libs.cursor.add("enemy","assets/hud/cursors/enemy.png")
-  libs.cursor.add("neutral","assets/hud/cursors/neutral.png")
+  loader:add("cursors",function()
+    libs.cursor.add("default","assets/hud/cursors/default.png")
+    libs.cursor.add("player","assets/hud/cursors/player.png")
+    libs.cursor.add("enemy","assets/hud/cursors/enemy.png")
+    libs.cursor.add("neutral","assets/hud/cursors/neutral.png")
 
-  libs.cursor.add("crew","assets/hud/cursors/crew.png")
-  libs.cursor.add("material","assets/hud/cursors/material.png")
-  libs.cursor.add("ore","assets/hud/cursors/ore.png")
+    libs.cursor.add("crew","assets/hud/cursors/crew.png")
+    libs.cursor.add("material","assets/hud/cursors/material.png")
+    libs.cursor.add("ore","assets/hud/cursors/ore.png")
 
-  libs.cursor.add("follow","assets/hud/cursors/follow.png")
-  libs.cursor.add("shoot","assets/hud/cursors/shoot.png")
-  libs.cursor.add("takeover","assets/hud/cursors/takeover.png")
+    libs.cursor.add("follow","assets/hud/cursors/follow.png")
+    libs.cursor.add("shoot","assets/hud/cursors/shoot.png")
+    libs.cursor.add("takeover","assets/hud/cursors/takeover.png")
 
-  libs.cursor.change("default")
-  libs.cursor.mode(settings:read("mouse_draw_mode"))
+    libs.cursor.change("default")
+    libs.cursor.mode(settings:read("mouse_draw_mode"))
+  end)
 
+  loader:add("args",function()
   local version_server_check = true
-
-  for i,v in pairs(arg) do
-    if v == "novn" then
-      disable_vn = true
-    end
-    if states[v] then
-      target_state = states[v]
-    end
-    if v == "operationcwal" then
-      cheat_operation_cwal = true
-    end
-    if v == "cheat" then
-      cheat = true
-      settings:write("tree_points",9000)
-    end
-    if v == "debug" then
-      debug_mode = true
-    end
-    if v == "nocheck" then
-      version_server_check = false
-    end
-    if v == "nodraw" then
-      function love.graphics.draw(...)
-        local arg={...}
-        if arg[2] and type(arg[2]) == "userdata" then
-          --love.graphics.drawq(unpack(arg))
-        else
-          love.graphics.drawd(unpack(arg))
+    for i,v in pairs(arg) do
+      if v == "novn" then
+        disable_vn = true
+      end
+      if states[v] then
+        target_state = states[v]
+      end
+      if v == "operationcwal" then
+        cheat_operation_cwal = true
+      end
+      if v == "cheat" then
+        cheat = true
+        settings:write("tree_points",9000)
+      end
+      if v == "debug" then
+        debug_mode = true
+      end
+      if v == "nocheck" then
+        version_server_check = false
+      end
+      if v == "nodraw" then
+        function love.graphics.draw(...)
+          local arg={...}
+          if arg[2] and type(arg[2]) == "userdata" then
+            --love.graphics.drawq(unpack(arg))
+          else
+            love.graphics.drawd(unpack(arg))
+          end
+        end
+        function love.graphics.drawd(drawable, x, y, r, sx, sy, ox, oy, kx, ky )
+          local nx,ny = (x or 0)-(ox or 0),(y or 0)-(oy or 0)
+          local nw,nh = drawable:getWidth()*(sx or 1),drawable:getHeight()*(sy or 1)
+          if r and r ~= 0 then
+            love.graphics.circle("line",nx+nw/2,ny+nh/2,math.min(nw,nh)/2)
+          else
+            love.graphics.rectangle("line",nx,ny,nw,nh)
+          end
         end
       end
-      function love.graphics.drawd(drawable, x, y, r, sx, sy, ox, oy, kx, ky )
-        local nx,ny = (x or 0)-(ox or 0),(y or 0)-(oy or 0)
-        local nw,nh = drawable:getWidth()*(sx or 1),drawable:getHeight()*(sy or 1)
-        if r and r ~= 0 then
-          love.graphics.circle("line",nx+nw/2,ny+nh/2,math.min(nw,nh)/2)
-        else
-          love.graphics.rectangle("line",nx,ny,nw,nh)
-        end
-      end
     end
-  end
 
-  if version_server_check then
-    local http = require"socket.http"
-    local version_server_payload = libs.json.encode({count=git_count,hash=git_hash})
-    local version_server_url = "http://50.116.63.25/roguecraftsquadron.com/version.php?i="
-    local r,e = http.request(version_server_url..version_server_payload)
-    version_server = e == 200 and libs.json.decode(r) or nil
-  end
+    if version_server_check then
+      local http = require"socket.http"
+      local version_server_payload = libs.json.encode({count=git_count,hash=git_hash})
+      local version_server_url = "http://50.116.63.25/roguecraftsquadron.com/version.php?i="
+      local r,e = http.request(version_server_url..version_server_payload)
+      version_server = e == 200 and libs.json.decode(r) or nil
+    end
+  end)
 
   -- this hack allows me to re-order when gamestate runs draw
   local callbacks = {'errhand', 'update'} -- no draw
@@ -279,11 +284,11 @@ function love.load(arg)
     callbacks[#callbacks+1] = k
   end
   libs.hump.gamestate.registerEvents(callbacks)
-  libs.hump.gamestate.switch(target_state or states.splash)
+  libs.hump.gamestate.switch(target_state or states.load)
 end
 
 function love.resize()
-  libs.stars:reload()
+  if libs.stars then libs.stars:reload() end
   states.mission:resize()
   states.client:resize()
 end
