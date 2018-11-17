926b59f2ec1b76799d8221ce83c8bf1793fd9116
Seppi <josefnpat@gmail.com>
2018-11-16 23:13:08 -0600

    Increasing material/ore amount

--- src/assets/mp_objects/asteroid/object.lua
+++ src/assets/mp_objects/asteroid/object.lua
@@ -2,7 +2,7 @@ return function()
   return {
     type = "asteroid",
     size = 32,
-    ore_supply = math.random(500,1500),
+    ore_supply = math.random(500,1500)*4,
     rotate = (math.random(0,1)*2-1)/10,
     pc = false,
   }
--- src/assets/mp_objects/scrap/object.lua
+++ src/assets/mp_objects/scrap/object.lua
@@ -2,7 +2,7 @@ return function()
   return {
     type = "scrap",
     size = 32,
-    material_supply = math.random(100,300),
+    material_supply = math.random(100,300)*4,
     rotate = (math.random(0,1)*2-1)/20,
     pc = false,
   }
