740824b1b372d763e53bab27b80bb68a61c3cac1
Seppi <josefnpat@gmail.com>
2019-09-03 00:47:02 -0500

    Remove RCS from fade in/out due to title in vnjson

--- src/libs/fade.lua
+++ src/libs/fade.lua
@@ -1,4 +1,4 @@
-local image = love.graphics.newImage("assets/loading.png")
+--local image = love.graphics.newImage("assets/loading.png")
 
 return function (alpha)
   alpha = math.max(0,math.min(255,alpha))
@@ -6,12 +6,14 @@ return function (alpha)
     local prev = {love.graphics.getColor()}
     love.graphics.setColor(0,0,0,alpha)
     love.graphics.rectangle("fill",0,0,love.graphics.getWidth(),love.graphics.getHeight())
+    --[[
     love.graphics.setColor(255,255,255,alpha)
     love.graphics.draw(
       image,
       (love.graphics.getWidth()-image:getWidth())/2,
       (love.graphics.getHeight()-image:getHeight())/2
     )
+    --]]
     love.graphics.setColor(prev)
   end
 end
